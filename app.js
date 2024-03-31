8d3ab4960338f35dfc1236ad9737c18236e42aee
Анастасия Бойко <anastasia151000@rambler.ru>
2024-04-01 00:50:38 +0300

    add menu drop and update main components

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const react_projects = document.querySelector(".react_projects");
 const landing_projects = document.querySelector(".landing_projects");
 const animations_projects = document.querySelector(".animations_projects");
 
+const btn_menu = document.querySelector('.bx-menu_main');
+const bx_menu_drop = document.querySelector('.bx-menu_drop');
+const top_menu = document.querySelector(".menu_drop_down");
+
+// btn_menu.addEventListener("click", ()=>{
+//     console.log("jnrhsnm")
+//     top_menu.classList.add("menu_drop_down_active");
+// })
+
+// bx_menu_drop.addEventListener("click", ()=>{
+//     console.log("jnrhsnm 2") 
+//     top_menu.classList.remove("menu_drop_down_active");
+// })
+
+
+btn_menu.addEventListener("click", ()=> {
+    top_menu.classList.toggle('drop');
+  }
+);
+
+bx_menu_drop.addEventListener("click", ()=> {
+    top_menu.classList.toggle('drop');
+  }
+);
+
 react_button.addEventListener('click', ()=>{
     removeActive();
     react_projects.style.display = 'flex';
